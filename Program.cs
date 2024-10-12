928788e2319a175746b4ced1c67d28517a777060
shinpaingmin <shinpaingmin@gmail.com>
2024-10-12 20:40:19 +0630

    upgraded the system (separating and calculating scores, adding extra credits)

--- Program.cs
+++ Program.cs
@@ -5,35 +5,40 @@ int[] sophiaScores = [90, 86, 87, 98, 100, 94, 90];
 int[] andrewScores = [92, 89, 81, 96, 90, 89];
 int[] emmaScores = [90, 85, 87, 98, 68, 89, 89, 89];
 int[] loganScores = [90, 95, 87, 88, 96, 96];
+/*
 int[] beckyScores = [92, 91, 90, 91, 92, 92, 92];
 int[] chrisScores = [84, 86, 88, 90, 92, 94, 96, 98];
 int[] ericScores = [80, 90, 100, 80, 90, 100, 80, 90];
 int[] gregorScores = [91, 91, 91, 91, 91, 91, 91];    
+*/
 
 // Student names
-string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 int[] studentScores = new int[10];
 
 string currentStudentLetterGrade = "";
 
 // Write the Report Header to the console
-Console.WriteLine("Student\t\tGrade\n");
+Console.WriteLine("Student\t\tExam Score\tOverall\tGrade\tExtra Cerdit\n");
 
 foreach(string name in studentNames)
 {
     string currentStudent = name;
+
     if(currentStudent == "Sophia")
         studentScores = sophiaScores;
-    
+       
     else if(currentStudent == "Andrew")
         studentScores = andrewScores;
-
+    
     else if(currentStudent == "Emma")
         studentScores = emmaScores;
-
+        
     else if(currentStudent == "Logan")
         studentScores = loganScores;
         
+
+    /*    
     else if (currentStudent == "Becky")
         studentScores = beckyScores;
 
@@ -45,15 +50,20 @@ foreach(string name in studentNames)
 
     else if (currentStudent == "Gregor")
         studentScores = gregorScores;
-        
+    */
+
     else
         continue;
 
-    // initialize/reset the sum of scored assignments
-    int sumAssignmentScores = 0;
+    int gradedExtraCreditAssignments = 0;
+
+    int sumExamScores = 0;
+    int sumExtraCreditScores = 0;
 
     // initialize/reset the calculated average of exam + extra credit scores
     decimal currentStudentGrade = 0;
+    decimal currentStudentExamScore = 0;
+    decimal currentStudentExtraCreditScore = 0;
 
     // initialize/ reset a counter for the number of assignments
     int gradedAssignments = 0;
@@ -65,14 +75,19 @@ foreach(string name in studentNames)
 
         if(gradedAssignments <= examAssignments)
             // add the exam score to the sum
-            sumAssignmentScores += score;   
+            sumExamScores = sumExamScores + score;
         else
-            // add the extra credit points to the sum - bonus points equal to 10% of an exam score
-            sumAssignmentScores += score / 10;
-        
+        {
+           
+            gradedExtraCreditAssignments += 1;
+            sumExtraCreditScores += score;
+        }
     }
 
-    currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
+    currentStudentExamScore = (decimal)(sumExamScores) / examAssignments;
+    currentStudentExtraCreditScore = (decimal)(sumExtraCreditScores) / gradedExtraCreditAssignments;
+
+    currentStudentGrade = (decimal)((decimal)sumExamScores + ((decimal)sumExtraCreditScores / 10)) / examAssignments;
 
     if(currentStudentGrade >= 97)
         currentStudentLetterGrade = "A+";
@@ -113,7 +128,7 @@ foreach(string name in studentNames)
     else 
         currentStudentLetterGrade = "F";
     
-    Console.WriteLine($"{currentStudent}:\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
+    Console.WriteLine($"{currentStudent}:\t\t{currentStudentExamScore}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}\t{currentStudentExtraCreditScore} ({(((decimal)sumExtraCreditScores / 10) / examAssignments)} pts)");
 }
 
 // required for running in VS Code (keeps the Output windows open to view results)
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dde2f672fae2be1f1c3cb9bf5382c95179cdd2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8416fa82941932116420b80f1d1c3a56d46da36e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
